5648da26e2712d840e30a4e4dde6ae8131b4a561
ruifernandees <dev.ruifernandes@gmail.com>
2020-06-04 15:38:12 -0300

    Front-end mobile: Navigation configured

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-// import { Feather as Icon } from '@expo/vector-icons'
+import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
     
+    const navigation = useNavigation();
+
+    function handleNavigateToPoints() {
+      navigation.navigate('Points');
+    }
+
     return (
         <ImageBackground 
             style={styles.container} 
@@ -18,11 +25,10 @@ const Home = () => {
             </View>
 
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleNavigateToPoints}>
                     <View style={styles.buttonIcon}>
                         <Text style={styles.textArrow}>
-                            {/* <Icon name="arrow-right" color="#FFF" size={24} /> */}
-                            >
+                            <Icon name="arrow-right" color="#FFF" size={24} />
                         </Text>
                     </View>
                     <Text style={styles.buttonText}>
@@ -40,8 +46,7 @@ const styles = StyleSheet.create({
     },
     container: {
       flex: 1,
-      padding: 32,
-      backgroundColor: '#F0F0F5'
+      padding: 32
     },
   
     main: {
