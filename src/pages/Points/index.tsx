78267bca0fabded9f4232e42369f8415e51c456b
ruifernandees <dev.ruifernandes@gmail.com>
2020-06-04 17:41:13 -0300

    Front-end mobile: map implementation finished

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Image, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
 
 const Points = () => {
@@ -11,6 +11,10 @@ const Points = () => {
 
     function handleNavigateBack() {
         navigation.goBack();
+    }
+
+    function handleNavigateToDetail() {
+        navigation.navigate('Detail');
     }
 
     return (
@@ -24,7 +28,30 @@ const Points = () => {
             <Text style={styles.description}>Encontre um ponto de coleta</Text>
 
             <View style={styles.mapContainer}>
-                <MapView style={styles.map} />
+                <MapView 
+                    style={styles.map} 
+                    initialRegion={{
+                        latitude: -9.6487473, 
+                        longitude: -35.7168681,
+                        latitudeDelta: 0.014,
+                        longitudeDelta: 0.014
+                    }} 
+                >
+                    <Marker 
+                        style={styles.mapMarker}
+                        onPress={handleNavigateToDetail}
+                        coordinate={{
+                            latitude: -9.6487473, 
+                            longitude: -35.7168681
+                        }} 
+                        
+                    >
+                        <View>
+                            <Image style={styles.mapMarkerContainer} source={{ uri: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
+                            <Text style={styles.mapMarkerTitle}>Mercado</Text>
+                        </View>
+                    </Marker>
+                </MapView>
             </View>
         </View>
         <View style={styles.itemsContainer}>
